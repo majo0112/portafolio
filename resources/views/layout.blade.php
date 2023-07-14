1ab6b9c739b6f748863a940240fdf3a1021a7ac0
Maria Jose <mariajosepavamedina0112@gmail.com>
2023-07-14 15:35:38 -0500

    layout modificado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,24 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'MJ Pava | Web developer')</title>       
       <!-- Favicons -->
-  <link rel="icon" href="/DevFolio/assets/img/fav-icon.png" type="image/x-icon">
+  <link rel="icon" href="https://portafolio-production-a902.up.railway.app/DevFolio/assets/img/fav-icon.png" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.0/css/all.min.css"> 
 
   <!-- Vendor CSS Files -->
-  <link href="/DevFolio/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/DevFolio/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="/DevFolio/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="/DevFolio/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
   <!-- Template Main CSS File -->
-  <link href="/DevFolio/assets/css/style.css" rel="stylesheet">
+  <link href="https://portafolio-production-a902.up.railway.app/DevFolio/assets/css/style.css" rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
   </head>
 <body>
      <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
-    <a href="{{ route('/') }}" class="logo"><img src="/DevFolio/assets/img/logo.png" alt="" class="img-fluid"></a>
+    <a href="{{ route('/') }}" class="logo"><img src="https://portafolio-production-a902.up.railway.app/DevFolio/assets/img/logo.png" alt="" class="img-fluid"></a>
 
       <nav id="navbar" class="navbar">
         <ul class="menu">
@@ -58,17 +58,22 @@
   </footer><!-- End  Footer -->
 
   <!-- Vendor JS Files -->
-  <script src="/DevFolio/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="/DevFolio/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="/DevFolio/assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="/DevFolio/assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="/DevFolio/assets/vendor/typed.js/typed.umd.js"></script>
-  <script src="/DevFolio/assets/vendor/php-email-form/validate.js"></script>
+  <script src="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="https://portafolio-production-a902.up.railway.app/DevFolio/assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="/js/app.js"></script>
+  <script src="https://portafolio-production-a902.up.railway.app/js/app.js"></script>
 
 </body>
 
 </html>
+
+
+
+
+
 
